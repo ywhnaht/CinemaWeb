0779f41a00903fb43ff2eb22af6a3ed41756d929
volequyen <qvo9110@gmail.com>
2024-04-12 12:27:41 +0700

    hehee

--- FE_Q/assets/js/userscript.js
+++ FE_Q/assets/js/userscript.js
@@ -1,8 +1,12 @@
 
-const infor = document.querySelector('.collapse.infor');
-const history = document.querySelector('.collapse.history');
-infor.addEventListener('click', function() {
-    if (history.style.display = 'block') {
-        history.style.display = 'none';
-    }
+const inforBtn = document.querySelector('.infor-btn');
+const historyBtn = document.querySelector('.history-btn');
+console.log(infor);
+const history = document.querySelector('.history');
+inforBtn.addEventListener('click', () => {
+   history.classList.remove('show');
+});
+
+historyBtn.addEventListener('click', () => {
+   document.querySelector('.show').classList.remove('show');
 });
