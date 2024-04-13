6a7d3eb62d6c0594b860f5db1cdead3990f3b191
volequyen <qvo9110@gmail.com>
2024-04-13 21:57:25 +0700

    fix được rùi đây

--- FE_Q/assets/js/userscript.js
+++ FE_Q/assets/js/userscript.js
@@ -1,18 +1,32 @@
-const inforBtn = document.querySelector('.infor-btn');
-const historyBtn = document.querySelector('.history-btn');
+const inforBtn = document.querySelectorAll('.infor-btn');
+const historyBtn = document.querySelectorAll('.history-btn');
 const updateBtn = document.querySelector('.update');
 const remove = document.querySelectorAll('.remove');  
-console.log(history);
 const history = document.querySelector('.history');
-inforBtn.addEventListener('click', () => {
-   history.classList.remove('show');
-});
 
-historyBtn.addEventListener('click', () => {
-   document.querySelector('.show').classList.remove('show');
-});
+// inforBtn.addEventListener('click', () => {
+//    history.classList.remove('show');
+// });
+
+// historyBtn.addEventListener('click', () => {
+//    document.querySelector('.show').classList.remove('show');
+// });
 updateBtn.addEventListener('click', () => {
    for(var input of remove) {
        input.classList.remove('remove');
    }
+});
+
+historyBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        document.querySelector('.show').classList.remove('show');
+    });
+   
+});
+
+inforBtn.forEach(element => {
+    element.addEventListener('click', () => {
+        history.classList.remove('show');
+    });
+   
 });
