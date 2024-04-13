4682c998b9efc06151ad7fa8ca66af3d35735e53
Huy Hồ Thanh <hothanhhuy24062004@gmail.com>
2024-04-13 15:20:12 +0700

    Revert "xoá mấy cái linh tinh"
    This reverts commit b03df9b7fbaf9e2007b9d0f43cc7791ebab42f82.

---
+++ Controllers/MoviesListController.cs
@@ -0,0 +1,33 @@
+﻿using CinemaWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaWeb.Controllers
+{
+    public class MoviesListController : Controller
+    {
+        Cinema_Web_Entities db = new Cinema_Web_Entities();
+        // GET: MoviesList
+        public ActionResult MoviesList()
+        {
+            List<movy> movielist = db.movies.ToList();
+            DateTime currentDate = DateTime.Now.Date;
+            foreach (var movie in movielist)
+            {
+                if (movie.release_date <= currentDate && movie.end_date >= currentDate)
+                {
+                    movie.movie_status = true; // Đang chiếu
+                }
+                else
+                {
+                    movie.movie_status = false; // Sắp chiếu
+                }
+            }
+            ViewBag.MovieList = movielist;
+            return View();
+        }
+    }
+}
