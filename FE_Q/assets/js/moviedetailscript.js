09fb943218408fcb27c2fcb687c83a8033717dec
volequyen <qvo9110@gmail.com>
2024-04-16 01:33:38 +0700

    còn vài chỗ chưa fix nma commit lên chạy kpi

---
+++ FE_Q/assets/js/moviedetailscript.js
@@ -0,0 +1,55 @@
+const mdBtn = document.querySelector('.monday-btn');
+const tdBtn = document.querySelector('.tuesday-btn');
+const wdBtn = document.querySelector('.wednesday-btn');
+const thdBtn = document.querySelector('.thursday-btn');
+const fdBtn = document.querySelector('.friday-btn');
+const sdBtn = document.querySelector('.saturday-btn');
+const sudBtn = document.querySelector('.sunday-btn');
+
+mdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+
+tdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+
+wdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+
+thdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+
+fdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+sdBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+sudBtn.addEventListener('click',() => {
+    current = document.querySelector('.show');
+    if(current){
+        current.classList.remove('show');
+    }
+});
+
