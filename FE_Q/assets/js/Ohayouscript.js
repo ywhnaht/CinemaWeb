af7df39239c4dec3a64c4c89ba2e81304570e102
volequyen <qvo9110@gmail.com>
2024-05-05 14:05:15 +0700

    xong giao diện

---
+++ FE_Q/assets/js/Ohayouscript.js
@@ -0,0 +1,55 @@
+const tdBtn = document.querySelector('.today-btn');
+const tmBtn = document.querySelector('.tomorow-btn');
+const datBtn = document.querySelector('.dat-btn');
+
+tdBtn.addEventListener('click', () => {
+    const currentTab = document.querySelector('.tab-pane.show');
+    if (currentTab) {
+        currentTab.classList.remove('show');
+    }
+    const targetTab = document.querySelector('#today');
+    targetTab.classList.add('show');
+
+    const temp = document.querySelector('.nav-link.active');
+    if (temp) {
+        temp.classList.remove('active');
+        temp.classList.add('default');
+    }
+    tdBtn.classList.remove('default');
+    tdBtn.classList.add('active');
+});
+
+tmBtn.addEventListener('click', () => {
+    const currentTab = document.querySelector('.tab-pane.show');
+    if (currentTab) {
+        currentTab.classList.remove('show');
+    }
+    const targetTab = document.querySelector('#tomorow');
+    targetTab.classList.add('show');
+    
+
+    const temp = document.querySelector('.nav-link.active');
+    if (temp) {
+        temp.classList.remove('active');
+        temp.classList.add('default');
+    }
+    targetTab.classList.remove('default');
+    targetTab.classList.add('active');
+});
+
+datBtn.addEventListener('click', () => {
+    const currentTab = document.querySelector('.tab-pane.show');
+    if (currentTab) {
+        currentTab.classList.remove('show');
+    }
+    const targetTab = document.querySelector('#day-after-tomorrow');
+    targetTab.classList.add('show');
+
+    const temp = document.querySelector('.nav-link.active');
+    if (temp) {
+        temp.classList.remove('active');
+        temp.classList.add('default');
+    }
+    targetTab.classList.remove('default');
+    targetTab.classList.add('active');
+});
