ce08c6ef461104b558aa153417ab6e4f5f34ac41
Huy Thanh <hothanhhuy24062004@gmail.com>
2024-05-09 00:52:41 +0700

    sửa giao diện Admin

---
+++ Areas/Admin/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CinemaWeb.Areas.Admin.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Admin/Movie
+
+        public ActionResult AllMovie()
+        {
+            return View();
+        }
+        public ActionResult AddMovie()
+        {
+            return View();
+        }
+
+        public ActionResult RemoveMovie()
+        {
+            return View();
+        }
+        public ActionResult EditMovie()
+        {
+            return View();
+        }
+    }
+}
