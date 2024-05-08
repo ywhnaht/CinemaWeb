f00d4db7d88520b3476f0f35467200924b62e9d1
volequyen <qvo9110@gmail.com>
2024-05-08 22:13:43 +0700

    thêm đánh giá

--- FE_Q/assets/js/moviedetailscript.js
+++ FE_Q/assets/js/moviedetailscript.js
@@ -6,6 +6,7 @@ const fdBtn = document.querySelector('.friday-btn');
 const sdBtn = document.querySelector('.saturday-btn');
 const sudBtn = document.querySelector('.sunday-btn');
 
+
 mdBtn.addEventListener('click',() => {
     current = document.querySelector('.show');
     if(current){
@@ -103,4 +104,29 @@ function stopVideo() {
 document.addEventListener('DOMContentLoaded', function() {
     var modal = document.getElementById('trailer');
     modal.addEventListener('hidden.bs.modal', stopVideo);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const stars = document.querySelectorAll('.star');
+    const confirmBtn = document.querySelector('.confirm');
+    stars.forEach(function(star) {
+        star.addEventListener('click', function() {
+            const isActive = this.classList.contains('star-active');
+            // Remove 'star-active' class from all stars
+            stars.forEach(function(s) {
+                s.classList.remove('star-active');
+            });
+            // Add 'star-active' class only to the clicked star if it was not active
+            if(!isActive){
+                this.classList.add('star-active');
+            }
+            // Add 'confirm-active' class to confirm button if there is at least one active star
+            const activeStars = document.querySelectorAll('.star-active');
+            if(activeStars.length > 0){
+                confirmBtn.classList.add('confirm-active');
+            } else {
+                confirmBtn.classList.remove('confirm-active');
+            }
+        });
+    });
 });
