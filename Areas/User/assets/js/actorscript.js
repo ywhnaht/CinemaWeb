18ebca887ee7ca36058c041f46d895d272a5dbdd
Huy Thanh <hothanhhuy24062004@gmail.com>
2024-05-21 00:18:15 +0700

    thêm quyền truy cập cho guest

--- Areas/User/assets/js/actorscript.js
+++ Areas/User/assets/js/actorscript.js
@@ -10,3 +10,48 @@ optionBtns.forEach((btn) => {
         btn.classList.toggle('active');
     });
 });
+
+const login = document.querySelector('.js-login-btn');
+const modal = document.getElementById('js-modal');
+const container = document.getElementById('js-login-container');
+const registerBtn = document.getElementById('js-register');
+const loginBtn = document.getElementById('js-login');
+const signupBtn = document.querySelector('.js-signup');
+const signinBtn = document.querySelector('.js-signin');
+const inputClicked = document.querySelectorAll('.form-container input');
+const error_mess = document.querySelectorAll('.error-message');
+
+
+
+login.addEventListener('click', () => {
+    modal.classList.add('modal-open');
+});
+
+modal.addEventListener('click', () => {
+    modal.classList.remove('modal-open');
+    container.classList.remove("modal-active");
+});
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("modal-active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("modal-active");
+});
+
+container.addEventListener('click', (event) => {
+    event.stopPropagation()
+});
+
+//signinBtn.addEventListener('click', () => {
+//    modal.classList.remove('open');
+//});
+
+for (var inputs of inputClicked) {
+    inputs.addEventListener('click', () => {
+        for (var error of error_mess) {
+            error.textContent = "";
+        }
+    });
+}
