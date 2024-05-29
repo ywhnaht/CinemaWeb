c07bdbf9c309d56b814e9ffdc16c93b970dd59ce
hlewluv <153503120+hlewluv@users.noreply.github.com>
2024-05-30 00:43:29 +0700

    Update ScheduleDetailViewModel.cs

--- Models/ScheduleDetailViewModel.cs
+++ Models/ScheduleDetailViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +7,12 @@ namespace CinemaWeb.Models
 {
     public class ScheduleDetailViewModel
     {
+        public int ScheduleDetailId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string MovieName { get; set; }
+        public string RoomName { get; set; }
+        public DateTime MovieDisplayDate { get; set; }
+        public TimeSpan? ScheduleTime { get; set; }
     }
 }
