f6c90479c27ef04e1fc5ec1d8cffe0c805a85adf
hlewluv <153503120+hlewluv@users.noreply.github.com>
2024-05-30 00:44:05 +0700

    Update ScheduleDetailCreateViewModel.cs

--- Models/ScheduleDetailCreateViewModel.cs
+++ Models/ScheduleDetailCreateViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +7,12 @@ namespace CinemaWeb.Models
 {
     public class ScheduleDetailCreateViewModel
     {
+        public int movie_display_date_id { get; set; }
+        public int schedule_id { get; set; }
+        public DateTime start_time { get; set; }
+        public DateTime end_time { get; set; }
+        public int room_id { get; set; }
+        public int movie_id { get; set; }
+        public int display_date_id { get; set; }
     }
 }
