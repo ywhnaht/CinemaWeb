203be837b5f22631452257b909e8cefb42be0873
hlewluv <153503120+hlewluv@users.noreply.github.com>
2024-06-12 13:16:25 +0700

    Update UpLoadController.cs

--- Areas/Admin/Controllers/UpLoadController.cs
+++ Areas/Admin/Controllers/UpLoadController.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
+using System.IO;
 using System.Web;
 using System.Web.Mvc;
+using CinemaWeb.Areas.Admin.Helper;
 
 namespace CinemaWeb.Areas.Admin.Controllers
 {
-    public class UpLoadController : Controller
+    public class UploadController : Controller
     {
-        // GET: Admin/UpLoad
-        public ActionResult Index()
+        [HttpPost]
+        public ActionResult UploadImage(HttpPostedFileBase file)
         {
-            return View();
+            if (file != null && file.ContentLength > 0)
+            {
+                try
+                {
+                    var uploadsDir = Server.MapPath("~/up");
+                    if (!Directory.Exists(uploadsDir))
+                    {
+                        Directory.CreateDirectory(uploadsDir);
+                    }
+
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(uploadsDir, fileName);
+
+                    file.SaveAs(filePath);
+
+                    var baseUrl = $"{Request.Url.Scheme}://{Request.Url.Authority}";
+                    var publicUrl = $"{baseUrl}/up/{fileName}";
+
+                    return Json(new { success = true, url = publicUrl });
+                }
+                catch (Exception ex)
+                {
+                    return Json(new { success = false, error = ex.Message });
+                }
+            }
+            else
+            {
+                return Json(new { success = false, error = "Không có tệp nào được tải lên hoặc tệp trống." });
+            }
         }
     }
 }
